cc0e54801f016f7ce5d7f9e0be4f5fe0d907bd21
Aasim Sajjad <aasimsajjad22@gmail.com>
2023-02-26 13:55:51 +0500

    EP: 05 - The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,22 @@
             </div>
         </div>
 
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say ?" rows="5"></textarea>
+                        </div>
+
+                        <button class="btn btn-default" type="submit">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">Sign up/Login </a> to participate in this discussion</p>
+        @endif
+
     </div>
 @endsection
