72bb2e39a7c2850198a6b693fe07b8c0bd4c9277
Aasim Sajjad <aasimsajjad22@gmail.com>
2023-03-06 00:06:38 +0500

    EP:30 - Recording favouriting reply activity

--- app/Favourite.php
+++ app/Favourite.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favourite extends Model
 {
+    use RecordsActivity;
     protected $guarded = [];
+
+    public function favourited()
+    {
+        return $this->morphTo();
+    }
+
+    public function activity()
+    {
+        return $this->morphMany('App\Activity', 'subject');
+    }
 }
